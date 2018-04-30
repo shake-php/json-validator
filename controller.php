984c2710e82e9c17d3a68d8623b22e26bb37f71d
inisik <inisik0820@gmail.com>
2018-04-30 18:31:19 -0400

    Add the controller function.

--- controller.php
+++ controller.php
@@ -1,1 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * Shake Json Validator.
+ * Better php code equals better web applications.
+ *
+ * @license <https://github.com/shake-php/json-validator/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/json-validator>.
+ */
 
+function validate_shake_json(string $jsonText): bool {
+    return ShakeValidator::valid($jsonText);
+}
