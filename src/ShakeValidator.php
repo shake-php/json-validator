c0a5bfa2892652060dfc8112b7e8b51855a43cf3
inisik <inisik0820@gmail.com>
2018-04-30 18:40:22 -0400

    Add the validator class.

---
+++ src/ShakeValidator.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * Shake Json Validator.
+ * Better php code equals better web applications.
+ *
+ * @license <https://github.com/shake-php/json-validator/blob/master/LICENSE>.
+ * @link    <https://github.com/shake-php/json-validator>.
+ */
+
+/**
+ * @class ShakeValidator.
+ */
+class ShakeValidator
+{
+}
